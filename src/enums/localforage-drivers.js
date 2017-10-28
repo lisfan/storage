df038db65a0272c89874cdd274a868fdeec7b7bc
lisfan <goolisfan@gmail.com>
2017-10-28 22:00:04 +0800

    docs: 更新其他注释

--- src/enums/localforage-drivers.js
+++ src/enums/localforage-drivers.js
@@ -1,11 +1,10 @@
-/**
- * storage命名空间
- * @enum
- */
-
 import localforage from 'localforage'
 import sessionStorageWrapper from '../utils/localforage-sessionstoragewrapper'
 
+/**
+ * localforage 驱动器常量枚举
+ * @enum
+ */
 export default {
   SESSIONSTORAGE: sessionStorageWrapper._driver,
   LOCALSTORAGE: localforage.LOCALSTORAGE,
